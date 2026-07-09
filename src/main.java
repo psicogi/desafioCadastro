62973fa0016df057caed64512a0b31fa6189405f
Giovania Dantas <fabianagiovana321@gmail.com>
2026-07-08 22:17:24 -0300

    feat: add main menu with input validation

--- src/main.java
+++ src/main.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String opc;
+        do {
+            System.out.println("-----------------------------------------------------");
+            System.out.println("|                     MENU                          |");
+            System.out.println("-----------------------------------------------------");
+            System.out.println("1. Cadastrar um novo pet");
+            System.out.println("2. Alterar os dados do pet cadastrado");
+            System.out.println("3. Deletar um pet cadastrado");
+            System.out.println("4. Listar todos os pets cadastrados");
+            System.out.println("5. Listar pets por algum critério (idade, nome, raça)");
+            System.out.println("6. Sair");
+            System.out.println("-----------------------------------------------------");
+
+            opc = sc.nextLine();
+
+        } while (!opc.matches("[1-6]"));
+
+        sc.close();
+    }
 }
